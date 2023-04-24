d7eee3561f296c2cc39646c3959a33a2702591c0
엘풍 <eruhoon@gmail.com>
2023-04-24 23:39:16 +0900

    add DonationSource

--- src/common/network/NetworkModel.d.ts
+++ src/common/network/NetworkModel.d.ts
@@ -5,4 +5,16 @@ export interface NetworkModel {
 
 export type LoginParam = { type: 'channel' | 'user'; privateKey: string };
 
-export type DonationRegisterParam = { from: string; to: string; src: {} };
+export type DonationRegisterParam = {
+  from: string;
+  to: string;
+  src: DonationSource;
+};
+
+export type DonationSource =
+  | TypedDonationSource<'text', TextData>
+  | TypedDonationSource<'youtube', YoutubeData>;
+
+type TypedDonationSource<T, D> = { type: T; data: D };
+type TextData = { text: string };
+type YoutubeData = { link: string };
