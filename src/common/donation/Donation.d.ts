ecc8735bca653f1e8b69bf16f0e7ec3171583b85
엘풍 <eruhoon@gmail.com>
2024-01-19 20:33:25 +0900

    udpate Donation Type

--- src/common/donation/Donation.d.ts
+++ src/common/donation/Donation.d.ts
@@ -1,9 +1,27 @@
-export type Donation = TextDonation | YoutubeDonation;
+export type Donation = TextDonation | YoutubeDonation | VideoDonation;
 
-type TypedDonation<T, D> = { type: T; data: D };
+type TypedDonation<T, D> = {
+  key: string;
+  recipientKey: string;
+  senderKey: string;
+  print: boolean;
+  userName: string;
+  type: T;
+  data: D;
+};
 
 type TextData = { text: string };
 type YoutubeData = { link: string };
+type VideoData = {
+  type: string;
+  url: string;
+  limit: number;
+  id: string;
+  title: string;
+  start: number;
+  duration: number;
+};
 
 export type TextDonation = TypedDonation<'text', TextData>;
 export type YoutubeDonation = TypedDonation<'youtube', YoutubeData>;
+export type VideoDonation = TypedDonation<'video', VideoData>;
