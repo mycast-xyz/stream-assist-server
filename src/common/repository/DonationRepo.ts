8260f6066b916f75780dde94895aab6dc23f4e81
엘풍 <eruhoon@gmail.com>
2024-01-19 20:52:08 +0900

    fix bug

--- src/common/repository/DonationRepo.ts
+++ src/common/repository/DonationRepo.ts
@@ -3,7 +3,6 @@ import { Entry, Repo } from './Repo';
 export class DonationRepo extends Repo<DonationEntry> {}
 
 export type DonationEntry = {
-  hash: string;
   recipientUserKey: string;
   senderUserKey: string;
   donationPrint: boolean;
